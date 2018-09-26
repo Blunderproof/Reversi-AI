89b6786d4f3c24eb3c5c90fcd0f91ba82e6ddcfd
Clay Coleman <coleclayman@gmail.com>
2018-09-26 16:28:43 -0600

    Formatting and some other stuff too

--- ReversiAI_1/AI1.java
+++ ReversiAI_1/AI1.java
@@ -11,9 +11,9 @@ import java.text.*;
 class AI1 {
 
     public Socket s;
-	public BufferedReader sin;
-	public PrintWriter sout;
-    //Random generator = new Random();
+    public BufferedReader sin;
+    public PrintWriter sout;
+    // Random generator = new Random();
 
     double t1, t2;
     int me;
@@ -21,27 +21,27 @@ class AI1 {
     int state[][] = new int[8][8]; // state[0][0] is the bottom left corner of the board (on the GUI)
     int turn = -1;
     int round;
-    
+
     int validMoves[] = new int[64];
     int numValidMoves;
 
-    //ADDED 
+    // ADDED
     double defaultHScore[][] = new double[8][8];
     double moveValue[][] = new double[8][8]; // the number of tokens that will be gained by the move. 0 if invalid
     final int MAX_DEPTH = 3;
-    
+
     public AI1(int _me, String host) {
         me = _me;
         initClient(host);
-        
+
         initHScores(1.0, .4, -.5, .1);
 
         int myMove;
-        
+
         while (true) {
             System.out.println("Read");
             readMessage();
-            
+
             if (turn == me) {
                 System.out.println("Move");
 
@@ -50,22 +50,20 @@ class AI1 {
 
                 // minimax and alpha beta happen in here
                 myMove = getBestMoveUsingMinMax(parent);
-                
+
                 String sel = validMoves[myMove] / 8 + "\n" + validMoves[myMove] % 8;
-                
+
                 System.out.println("Selection: " + validMoves[myMove] / 8 + ", " + validMoves[myMove] % 8);
-                
+
                 sout.println(sel);
             }
         }
     }
 
-    public void buildChildNodes(RNode parent, int depth){
-        int currValidMoves[] = new int[64];
-
-        currValidMoves = getCurrValidMoves(round, parent.getState(), parent.getPlayer());
+    public void buildChildNodes(RNode parent, int depth) {
+        int[] currValidMoves = getCurrValidMoves(round, parent.getState(), parent.getPlayer());
         Map<Integer, Double> currMoveScores = calcMoveScores(parent, currValidMoves);
-        
+
         for (int i = 0; i < currValidMoves.length; i++) {
             int childPlayer = getChildPlayerFromPlayerAndDepth(parent.getPlayer(), parent.getDepth());
             double childScore = parent.getNetScore() + currMoveScore.get(parent.getMove()) * addOrSubtractForPlayer(childPlayer);
@@ -73,18 +71,18 @@ class AI1 {
                 new RNode(parent, depth + 1, applyMoveToState(validMoves[i], state), childScore, childPlayer)
             );
         }
-        
-        if (depth <= MAX_DEPTH) { 
+
+        if (depth <= MAX_DEPTH) {
             // build another layer of children
-            for(RNode child : parent.getChildren()){
+            for (RNode child : parent.getChildren()) {
                 buildChildNodes(child, child.getDepth());
             }
-        } 
-        
+        }
+
     }
 
-    public int[][] applyMoveToState(int move, int[][] currState){
-        int[][] newState = currState;
+    public int[][] applyMoveToState(int move, int[][] currState) {
+        int[][] newState = currState.clone();
         int row = move / 8;
         int col = move % 8;
 
@@ -93,8 +91,9 @@ class AI1 {
             for (incy = -1; incy < 2; incy++) {
                 if ((incx == 0) && (incy == 0))
                     continue;
-            
-                newState = updateState(newState, row, col, incx, incy, turn); //updated multiple times as the update can be from 1+ directions
+
+                // updated multiple times as the update can be from 1+ directions
+                newState = updateState(newState, row, col, incx, incy, turn); 
             }
         }
 
@@ -106,33 +105,32 @@ class AI1 {
         int sequence[] = new int[7];
         int seqLen;
         int i, r, c;
-        
+
         seqLen = 0;
         for (i = 1; i < 8; i++) {
-            r = row+incy*i;
-            c = col+incx*i;
-        
+            r = row + incy * i;
+            c = col + incx * i;
+
             if ((r < 0) || (r > 7) || (c < 0) || (c > 7))
                 break;
-        
+
             sequence[seqLen] = currState[r][c];
             seqLen++;
         }
-        
+
         int count = 0;
         for (i = 0; i < seqLen; i++) {
             if (turn == 0) {
                 if (sequence[i] == 2)
-                    count ++;
+                    count++;
                 else {
                     if ((sequence[i] == 1) && (count > 0))
                         count = 20;
                     break;
                 }
-            }
-            else {
+            } else {
                 if (sequence[i] == 1)
-                    count ++;
+                    count++;
                 else {
                     if ((sequence[i] == 2) && (count > 0))
                         count = 20;
@@ -140,91 +138,97 @@ class AI1 {
                 }
             }
         }
-        
+
         if (count > 10) {
             if (turn == 0) {
                 i = 1;
-                r = row+incy*i;
-                c = col+incx*i;
+                r = row + incy * i;
+                c = col + incx * i;
                 while (newState[r][c] == 2) {
                     newState[r][c] = 1;
                     i++;
-                    r = row+incy*i;
-                    c = col+incx*i;
+                    r = row + incy * i;
+                    c = col + incx * i;
                 }
-            }
-            else {
+            } else {
                 i = 1;
-                r = row+incy*i;
-                c = col+incx*i;
+                r = row + incy * i;
+                c = col + incx * i;
                 while (newState[r][c] == 1) {
                     newState[r][c] = 2;
                     i++;
-                    r = row+incy*i;
-                    c = col+incx*i;
+                    r = row + incy * i;
+                    c = col + incx * i;
                 }
             }
         }
         return newState;
     }
 
-    public Map<Integer, Double> calcMoveScores(RNode parent, int validMoves[]){
+    public Map<Integer, Double> calcMoveScores(RNode parent, int[] validMoves) {
         Map<Integer, Double> moveScores = new HashMap<Integer, Double>();
 
-        for(int move: validMoves){ //calculate what the scores are
-            // call check direction special, and take center row move the value there
-            getDirectionSpecial(moveScores);
-            moveScores[move] += currentLocationWorth; // <--- what?
-        }
+        // TODO
+        // for (int move : validMoves) { // calculate what the scores are
+        //     // call check direction special, and take center row move the value there
+        //     int score = 0;
+        //     for () {
+        //         for {
+        //             if (i == 0 && j == 0) {
+        //                 continue;
+        //             }
+        //             // do all of moves
+        //             score += getDirectionSpecial(moveScores);
+        //         }
+        //     }
+            
+        //     moveScores.put(move, score);
+        // }
         return moveScores;
     }
 
-    private void getDirectionSpecial(Map<Integer, Double>moveScores) { // <--- what?
-        if (parent.getState()) {
-            moveScores[row+','+col] += newScore;
+    private void getDirectionSpecial(Map<Integer, Double> moveScores) { // <--- what?
+        if (moveScores.get(move) != null) {
+            moveScores.put(move, moveScores.get(move) + newScore);
         } else {
-            moveScores[row+','+col] = newScore;
+            moveScores.put(move, newScore);
         }
     }
 
-
-    //create hScores
-    private void initHScores(double cornerScore, double edgeScore, double oneInScore, double normalScore){
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                if(i == 0 || i == 7 || j == 0 || j == 7){ // edges
+    // create hScores
+    private void initHScores(double cornerScore, double edgeScore, double oneInScore, double normalScore) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i == 0 || i == 7 || j == 0 || j == 7) { // edges
                     defaultHScore[i][j] = edgeScore;
-                }
-                else if((i == 1 || i == 6) && (j > 0 && j < 7) || (j == 1 || j == 6) && (i > 0 && i < 7) ){ // oneIn
+                } else if ((i == 1 || i == 6) && (j > 0 && j < 7) || (j == 1 || j == 6) && (i > 0 && i < 7)) { // oneIn
                     defaultHScore[i][j] = oneInScore;
-                }
-                else{
+                } else {
                     defaultHScore[i][j] = normalScore;
                 }
 
             }
-        } //explicitly set the corners
+        } // explicitly set the corners
         defaultHScore[0][0] = cornerScore;
         defaultHScore[0][7] = cornerScore;
         defaultHScore[7][0] = cornerScore;
         defaultHScore[7][7] = cornerScore;
     }
-    
 
     private int getBestMoveUsingMinMax(RNode parent) {
         // start with 0th child
-        int bestMove = parent.getChildren()[0].getMove();
-        double bestScore = getScoreOfBestChild(parent.getChildren()[0], Double.MIN_VALUE);
-        
+        int bestMove = parent.getChildren().get(0).getMove();
+        double bestScore = getScoreOfBestChild(parent.getChildren().get(0), Double.MIN_VALUE);
+
         for (int i = 1; i < parent.getChildren().length; i++) {
-            double currentScore = getScoreOfBestChild(parent.getChildren()[i], bestScore);
+            double currentScore = getScoreOfBestChild(parent.getChildren().get(i), bestScore);
             // at top level, we know this is going to be max
             if (currentScore > bestScore) {
                 bestScore = currentScore;
-                bestMove = parent.getChildren()[i].getMove();
-            }            
-        }                
-        
+                bestMove = parent.getChildren().get(i).getMove();
+            }
+        }
+
         return bestMove;
     }
 
@@ -233,15 +237,15 @@ class AI1 {
         if (node.getDepth() == MAX_DEPTH + 1) {
             return evaluateNode(node);
         }
-        
-        double bestScore = getScoreOfBestChild(node.getChildren()[0], worstScoreForPlayer(node.getPlayer()));
+
+        double bestScore = getScoreOfBestChild(node.getChildren().get(0), worstScoreForPlayer(node.getPlayer()));
         if (isScoreBetterThanBest(node.getPlayer(), bestScore, bestScoreSoFar)) {
             // pruning
             return bestScore;
         }
-        
+
         for (int i = 1; i < node.getChildren().length; i++) {
-            double currentScore = getScoreOfBestChild(node.getChildren()[i], bestScore);
+            double currentScore = getScoreOfBestChild(node.getChildren().get(i), bestScore);
             // at top level, we know this is going to be max
             if (isScoreBetterThanBest(node.getPlayer(), currentScore, bestScore)) {
                 bestScore = currentScore;
@@ -250,9 +254,9 @@ class AI1 {
                     // pruning
                     return bestScore;
                 }
-            }            
-        }     
-        
+            }
+        }
+
         return bestScore;
     }
 
@@ -275,43 +279,42 @@ class AI1 {
         return node.getNetScore();
     }
 
-    private int getCurrValidMoves(int round, int [][]pState, int player) {
+    private int getCurrValidMoves(int round, int[][] pState, int player) {
         int cValidMoves[] = new int[64];
         int cNumValidMoves = 0;
         player = me;
         int i, j;
-        
+
         // Game Start
         if (round < 4) {
             if (pState[3][3] == 0) {
-                cValidMoves[cNumValidMoves] = 3*8 + 3;
-                cNumValidMoves ++;
+                cValidMoves[cNumValidMoves] = 3 * 8 + 3;
+                cNumValidMoves++;
             }
             if (pState[3][4] == 0) {
-                cValidMoves[cNumValidMoves] = 3*8 + 4;
-                cNumValidMoves ++;
+                cValidMoves[cNumValidMoves] = 3 * 8 + 4;
+                cNumValidMoves++;
             }
             if (pState[4][3] == 0) {
-                cValidMoves[cNumValidMoves] = 4*8 + 3;
-                cNumValidMoves ++;
+                cValidMoves[cNumValidMoves] = 4 * 8 + 3;
+                cNumValidMoves++;
             }
             if (pState[4][4] == 0) {
-                cValidMoves[cNumValidMoves] = 4*8 + 4;
-                cNumValidMoves ++;
+                cValidMoves[cNumValidMoves] = 4 * 8 + 4;
+                cNumValidMoves++;
             }
             System.out.println("Valid Moves:");
             for (i = 0; i < cNumValidMoves; i++) {
                 System.out.println(cValidMoves[i] / 8 + ", " + cValidMoves[i] % 8);
             }
-        }
-        else {
+        } else {
             System.out.println("Valid Moves:");
             for (i = 0; i < 8; i++) {
                 for (j = 0; j < 8; j++) {
                     if (pState[i][j] == 0) {
                         if (couldBe(pState, i, j)) {
-                            cValidMoves[cNumValidMoves] = i*8 + j;
-                            cNumValidMoves ++;
+                            cValidMoves[cNumValidMoves] = i * 8 + j;
+                            cNumValidMoves++;
                             System.out.println(i + ", " + j);
                         }
                     }
@@ -321,41 +324,38 @@ class AI1 {
 
         return cValidMoves;
     }
-    
-
 
     // Already implemented, don't touch
-    private boolean checkDirection(int state[][], int row, int col, int incx, int incy) { //state, 2, 3, -1, -1
+    private boolean checkDirection(int state[][], int row, int col, int incx, int incy) { // state, 2, 3, -1, -1
         int sequence[] = new int[7];
         int seqLen;
         int i, r, c;
-        
+
         seqLen = 0;
         for (i = 1; i < 8; i++) { // i=3
-            r = row+incy*i; // r = -1 = 2 + (-1)*3
-            c = col+incx*i; // c = 0 = 3 + (-1)*3
-        
+            r = row + incy * i; // r = -1 = 2 + (-1)*3
+            c = col + incx * i; // c = 0 = 3 + (-1)*3
+
             if ((r < 0) || (r > 7) || (c < 0) || (c > 7))
                 break;
-        
+
             sequence[seqLen] = state[r][c]; // sequence[0, 0]
             seqLen++; // 2
         }
-        
+
         int count = 0;
         for (i = 0; i < seqLen; i++) { // i = 0
-            if (me == 1) { //if I'm player 1
+            if (me == 1) { // if I'm player 1
                 if (sequence[i] == 2) // if enemy territory
-                    count ++;
-                else { 
+                    count++;
+                else {
                     if ((sequence[i] == 1) && (count > 0))
                         return true;
                     break;
                 }
-            }
-            else { //if I'm player 2
+            } else { // if I'm player 2
                 if (sequence[i] == 1)
-                    count ++;
+                    count++;
                 else {
                     if ((sequence[i] == 2) && (count > 0))
                         return true;
@@ -363,47 +363,47 @@ class AI1 {
                 }
             }
         }
-        
+
         // not a valid move to take a piece
         return false;
     }
-    
+
     // Already implemented, don't touch
     private boolean couldBe(int state[][], int row, int col) {
         int incx, incy;
-        
+
         for (incx = -1; incx < 2; incx++) {
             for (incy = -1; incy < 2; incy++) {
                 if ((incx == 0) && (incy == 0))
                     continue;
-            
+
                 if (checkDirection(state, row, col, incx, incy))
                     return true;
             }
         }
-        
+
         return false;
     }
-    
+
     // Already implemented, don't touch
     public void readMessage() {
         int i, j;
         String status;
         try {
-            //System.out.println("Ready to read again");
+            // System.out.println("Ready to read again");
             turn = Integer.parseInt(sin.readLine());
-            
+
             if (turn == -999) {
                 try {
                     Thread.sleep(200);
                 } catch (InterruptedException e) {
                     System.out.println(e);
                 }
-                
+
                 System.exit(1);
             }
-            
-            //System.out.println("Turn: " + turn);
+
+            // System.out.println("Turn: " + turn);
             round = Integer.parseInt(sin.readLine());
             t1 = Double.parseDouble(sin.readLine());
             System.out.println(t1);
@@ -418,7 +418,7 @@ class AI1 {
         } catch (IOException e) {
             System.err.println("Caught IOException: " + e.getMessage());
         }
-        
+
         System.out.println("Turn: " + turn);
         System.out.println("Round: " + round);
         for (i = 7; i >= 0; i--) {
@@ -429,16 +429,16 @@ class AI1 {
         }
         System.out.println();
     }
-    
+
     // Already implemented, don't touch
     public void initClient(String host) {
-        int portNumber = 3333+me;
-        
+        int portNumber = 3333 + me;
+
         try {
-			s = new Socket(host, portNumber);
+            s = new Socket(host, portNumber);
             sout = new PrintWriter(s.getOutputStream(), true);
-			sin = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            
+            sin = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
             String info = sin.readLine();
             System.out.println(info);
         } catch (IOException e) {
@@ -465,7 +465,7 @@ class AI1 {
             // negative to subtract
             return -1;
         }
-    } 
+    }
 
     private double worstScoreForPlayer(int player) {
         if (player == me) {
@@ -482,7 +482,5 @@ class AI1 {
     public static void main(String args[]) {
         new AI1(Integer.parseInt(args[1]), args[0]);
     }
-    
+
 }
-
-
