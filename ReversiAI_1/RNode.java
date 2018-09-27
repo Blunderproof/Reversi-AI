ebb338b4740eb93b0430d6cf23020bfba6f8e24b
Clay Coleman <coleclayman@gmail.com>
2018-09-27 16:27:45 -0600

    add parent to RNode and safecore

--- ReversiAI_1/RNode.java
+++ ReversiAI_1/RNode.java
@@ -9,17 +9,23 @@ public class RNode {
     private double netScore;
     private int player;
     private int move;
+    private int safeCount;
 
-    public RNode(RNode parent, int depth, double netScore, int player, int move) {
+    public RNode(RNode parent, int depth, double netScore, int player, int move, int safeCount) {
         this.parent = parent;
         this.depth = depth;
         this.netScore = netScore;
         this.player = player;
         this.move = move;
+        this.safeCount = safeCount;
     }
 
     public void addChild(RNode child){
         children.add(child);
+    }
+
+    public RNode getParent() {
+        return parent;
     }
 
     public List<RNode> getChildren(){
@@ -40,6 +46,10 @@ public class RNode {
 
     public int getPlayer() {
         return player;
+    }
+
+    public int getSafeCount() {
+        return safeCount;
     }
 
 }
